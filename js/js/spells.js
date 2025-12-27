cfd7d88ba16793cc89ed85bb18ca2e4b5af9f567
mythicmoonboy <liowyanmoon8@gmail.com>
2025-12-27 14:59:35 +0800

    Add spells.js with spell definitions

---
+++ js/js/spells.js
@@ -0,0 +1,14 @@
+const spells = [
+  {
+    name: "Flicker",
+    cooldown: "120s",
+    image: "assets/placeholder.png",
+    description: "Blink a short distance."
+  },
+  {
+    name: "Retribution",
+    cooldown: "35s",
+    image: "assets/placeholder.png",
+    description: "Deals damage to creeps."
+  }
+];
